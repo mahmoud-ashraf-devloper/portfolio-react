bf6382e8c97f0c9023a7629faf9c01f7fb9bfa17
Mahmoud Ashraf <88296664+mahmoud-ashraf-devloper@users.noreply.github.com>
2022-08-15 09:12:50 -0700

    adding my cv

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -37,9 +37,9 @@ const Home = () => {
                         <span className='leading-snug text-indigo-500'>Full-Stack Developer</span>
                     </div>
                 </div>
-                <NavLink exact="true" activeclassname="active" className='cursor-pointer' to="/contact">
-                    <button className='md:w-1/4 md:text-2xl fade-in-3 mt-8 flex justify-center  whitespace-nowrap px-8 py-2 rounded-md tracking-wider from-indigo-500 to-purple-800 text-white bg-gradient-to-bl hover:bg-gradient-to-br hover:text-white/80 transition duration-300 ease-in'>CONTACT ME</button>
-                </NavLink>
+                <a href='/projects/mahmoud-frontend-dev-cv.pdf' download>
+                    <button className='download-cv-font md:w-1/4 md:text-2xl  font-mono text-indigo-100 fade-in-3 mt-8 flex justify-center  whitespace-nowrap px-8 py-2 rounded-md tracking-wider from-indigo-500 to-purple-800 bg-gradient-to-bl hover:bg-gradient-to-br hover:text-white/80 transition duration-300 ease-in-out uppercase'>Download My Cv</button>
+                </a>
             </div>
         </div>
 
