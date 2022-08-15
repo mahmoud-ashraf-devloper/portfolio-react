d2c833d902d9bd39f40269c9d0ee5f5a30246661
Mahmoud Ashraf <88296664+mahmoud-ashraf-devloper@users.noreply.github.com>
2022-08-15 08:18:25 -0700

    contact button fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AnimateLetters from './../AnimateLetters/AnimateLetters';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
     const data = {
@@ -16,7 +17,7 @@ const Home = () => {
 
     return (
         <div className='fade-in-2 home-font text-white md:pl-8 pl-3 min-h-[75vh] flex items-center '>
- 
+
             <div className=''>
                 <div className='md:text-5xl text-xl tracking-wider '>
                     <div className='py-2 flex flex-col'>
@@ -36,8 +37,9 @@ const Home = () => {
                         <span className='leading-snug text-indigo-500'>Full-Stack Developer</span>
                     </div>
                 </div>
-
-                <button className='fade-in-3 mt-8 flex whitespace-nowrap px-8 py-2 rounded-md tracking-wider from-indigo-500 to-purple-800 text-white bg-gradient-to-bl hover:bg-gradient-to-br hover:text-white/80 transition duration-300 ease-in'>CONTACT ME</button>
+                <NavLink exact="true" activeclassname="active" className='cursor-pointer' to="/contact">
+                    <button className='md:w-1/4 md:text-2xl fade-in-3 mt-8 flex justify-center  whitespace-nowrap px-8 py-2 rounded-md tracking-wider from-indigo-500 to-purple-800 text-white bg-gradient-to-bl hover:bg-gradient-to-br hover:text-white/80 transition duration-300 ease-in'>CONTACT ME</button>
+                </NavLink>
             </div>
         </div>
 
